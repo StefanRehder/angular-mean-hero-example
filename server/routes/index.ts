cb17950ecb5feadaf897de1f5a0afa4e7aebe7da
Stefan Rehder <rehder@monsenso.com>
2018-07-30 20:39:42 +0200

    fix

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -10,7 +10,6 @@ export class IndexRoutes {
         });
 
         app.get('*', (req: express.Request, res: express.Response) => {
-            // Not a defined path... Redirect to Angular app
             res.redirect('http://localhost:8200/');
         });
     }
